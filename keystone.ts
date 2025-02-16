89ddfbda79222ece091274cb2de7df53468c36b5
mishatsyhankou <Mikhail_Tsyhankou@epam.com>
2025-02-16 21:40:13 +0400

    Added project to github

---
+++ keystone.ts
@@ -0,0 +1,77 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+    withItemData,
+    statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
+import { CartItem } from './schemas/Cart';
+import { extendGraphqlSchema } from './mutations';
+
+const databaseURL =
+    process.env.DATABASE_URL ||
+    'mongodb://localhost/keystone-sick-fits-tutuorial';
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+    secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
+        // TODO: Add in inital roles here
+    },
+    passwordResetLink: {
+        async sendToken(args) {
+            await sendPasswordResetEmail(args.token, args.identity);
+        },
+    },
+});
+
+export default withAuth(
+    config({
+        // @ts-ignore
+        server: {
+            cors: {
+                origin: [process.env.FRONTEND_URL],
+                credentials: true,
+            },
+        },
+        db: {
+            adapter: 'mongoose',
+            url: databaseURL,
+            async onConnect(keystone) {
+                if (process.argv.includes('--seed-data')) {
+                    await insertSeedData(keystone);
+                }
+            },
+        },
+        lists: createSchema({
+            User,
+            Product,
+            ProductImage,
+            CartItem,
+        }),
+        extendGraphqlSchema,
+        ui: {
+            // Change for roles
+            isAccessAllowed: ({ session }) => {
+                console.log(session);
+                return !!session?.data;
+            },
+        },
+        // TODO session values
+        session: withItemData(statelessSessions(sessionConfig), {
+            User: 'id',
+        }),
+    })
+);
