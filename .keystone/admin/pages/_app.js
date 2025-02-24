95d336dee2af76014d2324e483af1efce73ecd63
mishatsyhankou <Mikhail_Tsyhankou@epam.com>
2025-02-24 22:57:11 +0400

    Added mutations for order

--- .keystone/admin/pages/_app.js
+++ .keystone/admin/pages/_app.js
@@ -12,6 +12,7 @@ import * as view0a84a4cb from '../../../node_modules/@keystone-next/fields/types
 import * as view6f167c6b from '../../../node_modules/@keystone-next/fields/types/select/views';
 import * as view815fe3ff from '../../../node_modules/@keystone-next/fields/types/integer/views';
 import * as view0666e214 from '../../../node_modules/@keystone-next/cloudinary/views';
+import * as view5400767d from '../../../node_modules/@keystone-next/fields/types/virtual/views';
 
 const adminConfig = {};
 
@@ -24,6 +25,7 @@ const fieldViews = {
   view6f167c6b,
   view815fe3ff,
   view0666e214,
+  view5400767d,
 };
 
 const lazyMetadataQuery = {
@@ -198,7 +200,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="wr66bi"
+        adminMetaHash="b4kz3e"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
--- keystone.ts
+++ keystone.ts
@@ -1,21 +1,24 @@
-import 'dotenv/config';
-import { config, createSchema } from '@keystone-next/keystone/schema';
 import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
 import {
     withItemData,
     statelessSessions,
 } from '@keystone-next/keystone/session';
-import { User } from './schemas/User';
-import { Product } from './schemas/Product';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
 import { ProductImage } from './schemas/ProductImage';
+import { Product } from './schemas/Product';
+import { User } from './schemas/User';
+import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
-import { CartItem } from './schemas/Cart';
 import { extendGraphqlSchema } from './mutations';
+import { CartItem } from './schemas/Cart';
+
+function check(name: string) { }
 
 const databaseURL =
-    process.env.DATABASE_URL ||
-    'mongodb://localhost/keystone-sick-fits-tutuorial';
+    process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
     maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
@@ -32,6 +35,7 @@ const { withAuth } = createAuth({
     },
     passwordResetLink: {
         async sendToken(args) {
+            // send the email
             await sendPasswordResetEmail(args.token, args.identity);
         },
     },
@@ -50,28 +54,31 @@ export default withAuth(
             adapter: 'mongoose',
             url: databaseURL,
             async onConnect(keystone) {
+                console.log('Connected to the database!');
                 if (process.argv.includes('--seed-data')) {
                     await insertSeedData(keystone);
                 }
             },
         },
         lists: createSchema({
+            // Schema items go in here
             User,
             Product,
             ProductImage,
             CartItem,
+            OrderItem,
+            Order,
         }),
         extendGraphqlSchema,
         ui: {
-            // Change for roles
-            isAccessAllowed: ({ session }) => {
-                console.log(session);
-                return !!session?.data;
-            },
+            // Show the UI only for poeple who pass this test
+            isAccessAllowed: ({ session }) =>
+                // console.log(session);
+                !!session?.data,
         },
-        // TODO session values
         session: withItemData(statelessSessions(sessionConfig), {
-            User: 'id',
+            // GraphQL Query
+            User: 'id name email',
         }),
     })
 );
